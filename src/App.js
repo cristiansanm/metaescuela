939cc76d37a42d95f6d8a802abb6af3d00a5a083
MLozano97 <drago8971@gmail.com>
2022-04-04 21:09:50 +0200

    Test | MET-4 | Test of git commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React xD
+          Learn React xD xd
         </a>
       </header>
     </div>
