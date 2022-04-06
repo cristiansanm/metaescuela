ccbb6f6119c52c5d90f860ec3a2c64ffebfedccf
Cristian Sanchez <krissanz13@gmail.com>
2022-04-06 22:01:47 +0200

    new example of components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TableBase from './components/CommonUiComponents/TableBase';
+
+function App() {
+  return (
+    <div>
+      <TableBase />
+    </div>
+  );
+}
+
+export default App;
