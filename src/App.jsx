c1702d5b2d58a6b940b7301157962d872cb20fb2
Richardhnb7 <richardhnb@hotmail.es>
2022-04-13 21:33:49 +0200

    creating new login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import TableBase from './components/CommonUiComponents/TableBase';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div>
-      <TableBase />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}></Route>
+          <Route path="tabla" component={<TableBase/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
